51fd897d02581320a139ea94dafcaadfded8f2df
luistf24 <luis.tormo24@gmail.com>
2023-05-11 12:27:35 +0200

    Modifico el script mide-tiempos

--- P4/mide-tiempos.sh
+++ P4/mide-tiempos.sh
@@ -3,21 +3,31 @@
 rm $2/data/$1.dat
 
 
-i=100
-tam=20000
-jump=1000
+i=4
+tam=20
+jump=1
 
-if [ "$1" == "voraz" ]
-then
-	i=1000
-	tam=100000
-	jump=3000
-fi
+#if [ "$1" == "voraz" ]
+#then
+	#i=1000
+	#tam=100000
+	#jump=3000
+#fi
 
 while [ "$i" -le $tam ]
 do
-	echo $i
-    $2/bin/$1 $i $3 $4 >> $2/data/$1.dat
-     i=$(( $i + $jump ))
+	echo "Tamaño : " $i
+	Generador/bin/generador $i
+
+	echo "Cota 1"
+    $2/bin/$1 $i 0 0 >> $2/data/$1_0.dat
+
+	echo "Cota 2"
+    $2/bin/$1 $i 1 0 >> $2/data/$1_1.dat
+
+	echo "Cota 3"
+    $2/bin/$1 $i 2 0 >> $2/data/$1_2.dat
+
+    i=$(( $i + $jump ))
 done
       
